1b9aaad965a91cb2ae9e46b9bd6ce3f6a1c91e41
danchern97 <danchern97@mail.ru>
2015-10-16 18:07:27 +0300

    Sorting out Eugene Onegin
    Sorted it using bubble sort, in future will try to use qsort.
    In Out.txt  at first goes strings with the lowest char number in ASCII
    table, so it possible not to be an 'A' letter or something.

---
+++ Eugene_Onegin_Sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 512
+
+void s_bubble(char** ar, int len);
+
+int main(){
+    FILE* in_file = fopen("Eugene Onegin.txt", "r");
+    FILE* out_file = fopen("Out.txt", "w");
+    char buff[LEN];
+    int len = 0, i = 0;
+    if(in_file == NULL){
+        fprintf(out_file, "Problems with in-file");
+        return 0;
+    }
+    while(fgets(buff, LEN, in_file) != NULL){
+        len++;
+    }
+    char** ar = (char**) malloc(sizeof(char*)*(len+1));
+    rewind(in_file);
+    while(fgets(buff, LEN, in_file) != NULL){
+        ar[i] = (char*) malloc((strlen(buff)+1)*sizeof(char));
+        strcpy(ar[i], buff);
+        i++;
+    }
+    i = 0;
+    s_bubble(ar, len);
+    while(i < len){
+        if(i % 2 == 0){
+            fprintf(out_file, "%s", ar[i]);
+        }
+        free(ar[i]);
+        i++;
+    }
+    free(ar);
+    fclose(in_file);
+    fclose(out_file);
+    return 0;
+}
+
+void s_bubble(char** ar, int len){
+    int i, j;
+    for(i = 0; i < len; i++){
+        for(j = 0; j < len; j++){
+            if(strcmp(ar[i], ar[j]) < 0){
+                char* t = ar[i];
+                ar[i] = ar[j];
+                ar[j] = t;
+            }
+        }
+    }
+}
