510a4143dd7d11036b9f3d12b094dbf72e8a412d
Даниил Чернявский <danchern97@mail.ru>
2016-03-15 19:17:28 +0300

    Переделал стек, теперь он нормально разнесён в хиддер и cpp файл.

--- stack.cpp
+++ stack.cpp
@@ -1,78 +1,73 @@
 #include <iostream>
 #include <cstdlib>
+#include "stack.h"
+#include <assert.h>
 
-typedef int stack_type;
-
-class CStack {
-private:
-
-    int size_d;
-    int pointer;
-    stack_type* date_d;
-
-    void isfull() {
-        if(pointer >= size_d) {
-            resize_d();
-        }
+void CStack::isfull() {
+    if(pointer >= size_d) {
+        resize_d();
     }
+}
 
-    int isempty() {
-        if(pointer < 0) {
-            return 1;
-        } else {
-            return 0;
-        }
+int CStack::isempty() {
+    if(pointer < 0) {
+        return 1;
+    } else {
+        return 0;
     }
+}
 
-    void resize_d() {
-        size_d += 100;
-        date_d = (stack_type*) realloc(date_d, size_d*sizeof(stack_type));
-    }
+void CStack::resize_d() {
+    size_d += 100;
+    date_d = (stack_type*) realloc(date_d, size_d*sizeof(stack_type));
+    assert(date_d);
+}
 
-public:
+CStack::CStack() {
+    size_d = 10;
+    pointer = -1;
+    date_d = (stack_type*) calloc(size_d, sizeof(stack_type));
+    assert(date_d);
+}
 
-    CStack() {
-        size_d = 10;
-        pointer = -1;
-        date_d = (stack_type*) calloc(size_d, sizeof(stack_type));
-    }
+CStack::CStack(int x) {
+    size_d = x;
+    pointer = -1;
+    date_d = (stack_type*) calloc(size_d, sizeof(stack_type));
+    assert(date_d);
+}
 
-    CStack(int x) {
-        size_d = x;
-        pointer = -1;
-        date_d = (stack_type*) calloc(size_d, sizeof(stack_type));
-    }
+CStack::~CStack() {
+    size_d = 0;
+    pointer = 0;
+    free(date_d);
+}
 
-    ~CStack() {
-        size_d = 0;
-        pointer = 0;
-        free(date_d);
-    }
 
-    void push(stack_type t) {
-        isfull();
-        pointer++;
-        date_d[pointer] = t;
-    }
+void CStack::push(stack_type t) {
+    isfull();
+    pointer++;
+    date_d[pointer] = t;
+}
 
-    stack_type pop() {
-        if(!isempty()) {
-            stack_type t = date_d[pointer];
-            date_d[pointer] = (stack_type) NULL;
-            pointer --;
-            return t;
-        } else {
-            std::cout « "Error: stack is empty" « std::endl;
-            return (stack_type) NULL;
-        }
-    }
+stack_type CStack::pop() {
+    assert((!isempty()));
+    stack_type t = date_d[pointer];
+    date_d[pointer] = (stack_type) NULL;
+    pointer --;
+    return t;
+}
 
-    void dump() {
+void CStack::dump() {
+    if(isempty()) {
+        std::cout << "Stack is empty" << std::endl;
+    } else {
         int i = pointer;
         while(i >= 0) {
-            std::cout « date_d[i] « std::endl;
+            std::cout << date_d[i] << std::endl;
             i--;
+
         }
     }
+}
 
-};
